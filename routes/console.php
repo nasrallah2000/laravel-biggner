5fbd8ba21c41d51320eab366b859232429efcd2d
Nasrallah <nsrallahh333@gmail.com>
2025-01-25 07:44:54 +0200

    route type

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('dev', function () {
+    $this->comment("Eng.Nasrallah Alsaifi");
+})->purpose('Display the developer name');
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+// The Type Of routes method
+// Route::get('url','action'); الروابط المتعلقة بعرض صفحات الموقع
+// Route::post('url','action'); الروابط المتعلقة باخذ بيانات من المستخدم
+// Route::put('url','action'); الروابط المسؤولة عن تعديل البيانات (احدث وتعدل عنصر واحد)
+// Route::patch('url','action'); الروابط المسؤولة عن تعديل البيانات (اقدم وتعدل اكثر من عنصر)
+// Route::delete('url','action'); الروابط المسؤولة عن حذف البيانات
+// Route::options('url','action'); مسؤول عن عرض الاخطاء المناسبة
+
 Route::get('/', function () {
     return "Home Page";
 });
